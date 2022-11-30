c894d551dd185c8562b78cf942f0536fbce56a67
zxcxv15 <zxcxv15@daum.net>
2022-11-30 19:21:39 +0900

    메뉴 리스트 불러오기 완료

--- src/main/java/kioskmake/projectkiosk/web/controller/PageController.java
+++ src/main/java/kioskmake/projectkiosk/web/controller/PageController.java
@@ -1,8 +1,13 @@
 package kioskmake.projectkiosk.web.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class PageController {
 
+	@GetMapping("/buger")
+	public String loadBugerPage() {
+		return "kiosk-main";
+	}
 }
