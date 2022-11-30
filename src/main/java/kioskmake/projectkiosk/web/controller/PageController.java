f6f82e030b7e243e3144541ba8525e13faf01715
hellojuhyeon <rkdwngus666@gmail.com>
2022-11-30 16:04:11 +0900

    pagecontroller작성/검토요망

--- src/main/java/kioskmake/projectkiosk/web/controller/PageController.java
+++ src/main/java/kioskmake/projectkiosk/web/controller/PageController.java
@@ -1,8 +1,48 @@
 package kioskmake.projectkiosk.web.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class PageController {
-
+	@GetMapping("/main")
+	public String mainpage() {
+		return "insert";
+	}
+	
+	@GetMapping("/kioskmain")
+	public String kioskmain() {
+		return "kioskmain";
+	}
+	
+	@GetMapping("/order")
+	public String order() {
+		return "order";
+	}
+	
+	@GetMapping("/payment")
+	public String payment() {
+		return "payment";
+	}
+	
+	@GetMapping("/set/select/view")
+	public String setselectview() {
+		return "setselectview";
+	}
+	
+	@GetMapping("/size/select/view")
+	public String sizeselectview() {
+		return "sizeselectview";
+	}
+	
+	@GetMapping("/basket")
+	public String basket() {
+		return "basket";
+	}
+	
+	@GetMapping("/tableservice")
+	public String tableservice() {
+		return "tableservice";
+	}
+	
 }
